f21411e2381c58913ae6efe18792ba9ae2638354
Mark Klimenko <22773@bk.ru>
2019-03-22 20:50:57 +0300

    web get rid of discovery

--- web/src/main/groovy/systems/vostok/humexp/pry/web/configuration/client/WebClientConfiguration.java
+++ web/src/main/groovy/systems/vostok/humexp/pry/web/configuration/client/WebClientConfiguration.java
@@ -1,7 +1,6 @@
 package systems.vostok.humexp.pry.web.configuration.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,20 +10,8 @@ public class WebClientConfiguration {
     @Autowired
     WebClientProperties properties;
 
-    /*@Bean
-    @LoadBalanced
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
-
     @Bean
-    public WebClient webClientSepa(@LoadBalanced WebClient.Builder webClientBuilder) {
-        return webClientBuilder.baseUrl(properties.getSepa().getHost()).build();
-    }*/
-
-    @Bean
-    @LoadBalanced
-    public WebClient.Builder loadBalancedWebClientBuilder() {
-        return WebClient.builder();
+    public WebClient webClientSepa() {
+        return WebClient.create(properties.getSepa().getHost());
     }
 }
--- web/src/main/groovy/systems/vostok/humexp/pry/web/service/document/SepaDocumentService.groovy
+++ web/src/main/groovy/systems/vostok/humexp/pry/web/service/document/SepaDocumentService.groovy
@@ -3,31 +3,21 @@ package systems.vostok.humexp.pry.web.service.document
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @Service
 @Slf4j
 class SepaDocumentService {
-    /*@Autowired
-    WebClient webClientSepa*/
-
     @Autowired
-    private WebClient.Builder webClientBuilder
+    WebClient webClientSepa
 
     Mono<String> createSepa(Mono<String> request) {
         log.info('WEB: act')
-        webClientBuilder.build().post()
-                .uri("http://agent-sepa/api/v2/document/build")
-                .body(request, String.class)
-                .retrieve()
-                .bodyToMono(String.class)
-
-        /*webClientSepa.post()
+        webClientSepa.post()
                 .uri('api/v2/document/build')
                 .body(request, String.class)
                 .retrieve()
-                .bodyToMono(String.class)*/
+                .bodyToMono(String.class)
     }
 }
